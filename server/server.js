18ca5618c8cd6c27da64a4fbce71fb2632140415
Soumya Suryan <soumyasuryan86@gmail.com>
2026-04-18 21:56:12 +0530

    server.js updated

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const dotenv = require('dotenv');
 const db = require('./config/db');
 
 dotenv.config();
-
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
 const testDbConnection = async () => {
   try {
     const res = await db.query('SELECT NOW()');
